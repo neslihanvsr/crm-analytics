bd87d79f7689a6a9052411da83046b54fc5aa80a
neslihan <neslihannavsar@gmail.com>
2022-10-31 14:36:36 +0300

    explanations

--- Customer Segmentation with RFM.py
+++ Customer Segmentation with RFM.py
@@ -9,12 +9,12 @@ df_ = pd.read_excel(r'C:\Users\güneş market\Desktop\exceldosy\online_retail_II
 
 def create_rfm(dataframe, csv=False):
 
-    #Veriyi Hazırlama
+    # Data Preparation
     dataframe['TotalPrice'] = dataframe['Quantity'] * dataframe['Price']
     dataframe.dropna(inplace=True)
     dataframe = dataframe[~dataframe['Invoice'].str.contains('C', na=False)]
 
-    #RFM Metriklerinin Hesaplanması
+    # Calculating RFM Metrics
     today_date = dt.datetime(2011, 12, 11)
     rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': lambda date: (today_date - date.max()).days,
                                                 'Invoice': lambda num: num.nunique(),
@@ -22,15 +22,15 @@ def create_rfm(dataframe, csv=False):
     rfm.columns = ['recency', 'frequency', 'monetary']
     rfm = rfm[(rfm['monetary'] > 0)]
 
-    # RFM Skorlarının Hesaplanması
+    # Calculating RFM Scores
     rfm['recency_score'] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
     rfm['frequency_score'] = pd.qcut(rfm['frequency'].rank(method='first'), 5, labels=[1, 2, 3, 4, 5])
     rfm['monetory_score'] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
 
-    # cltv_df skorları kategorik değere dönüştürülüp df'e eklendi:
+    # scores were converted to categorical values and added to df:
     rfm['RFM_SCORE'] = (rfm['recency_score'].astype(str)) + rfm['frequency_score'].astype(str)
 
-    # Segmentlerin İsimlendirilmesi
+    # Naming Segments (regex)
     seg_map = {
         r'[1-2][1-2]': 'hipernating',
         r'[1-2][3-4]': 'at_Risk',
